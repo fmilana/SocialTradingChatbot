5e8513e6ad4e4a19745313c1f3f633ba2103f685
Enrico Costanza <ec@ecs.soton.ac.uk>
2019-08-03 07:20:25 +0100

    added export functionality to the admin

--- chatbot/admin.py
+++ chatbot/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
+from import_export import resources
+from import_export.admin import ExportActionModelAdmin
 
-# Register your models here.
 
 from .models import (
     Profile,
@@ -15,13 +16,77 @@ from .models import (
     QuestionnaireResponse
     )
 
+class BalanceResource(resources.ModelResource):
+    class Meta:
+        model = Balance
+        fields = ['user', 'available', 'invested', 
+                    'user_username', 'user__participant__condition_active']
+
+class BalanceAdmin(ExportActionModelAdmin):
+    list_display = ['__str__', 'available', 'invested']
+    resource_class = BalanceResource
+
+
+class MessageResource(resources.ModelResource):
+    class Meta:
+        model = Message
+        fields = ['user', 'month', 'from_participant', 
+                    'from_notification', 'from_button', 
+                    'text', 'user_username', 'user__participant__condition_active']
+        
+class MessageAdmin(ExportActionModelAdmin):
+    #list_display = ['participant', 'participant__user__username', 'task', 'task__task_list__name']
+    #list_display = ['__str__', 'user__participant__condition_active']
+    list_display = ['__str__', 'user']
+    resource_class = MessageResource
+
+
+class ResultResource(resources.ModelResource):
+    class Meta:
+        model = Result
+        fields = ['month', 'profit', 'images_tagged', 'total', 
+                    'user_username', 'user__participant__condition_active']
+
+class ResultAdmin(ExportActionModelAdmin):
+    #list_display = ['participant', 'participant__user__username', 'task', 'task__task_list__name']
+    list_display = ['__str__', 'user']
+    resource_class = ResultResource
+
+
+class QuestionnaireResponseResource(resources.ModelResource):
+    class Meta:
+        model = QuestionnaireResponse
+        fields = ['user', 'answer', 'completion_time', 'subtask_time', 
+                    'created_at', 'updated_at', 
+                    'user_username', 'user__participant__condition_active']
+
+class QuestionnaireResponseAdmin(ExportActionModelAdmin):
+    #list_display = ['participant', 'participant__user__username', 'task', 'task__task_list__name']
+    list_display = ['__str__', 'user']
+    resource_class = QuestionnaireResponseResource
+
+
+class UserActionResource(resources.ModelResource):
+    class Meta:
+        model = UserAction
+        fields = ['user', 'month', 'available', 'invested', 
+                    'portfolio', 'chatbot_change', 'newspost_change', 
+                    'action', 'amount',
+                    'user_username', 'user__participant__condition_active']
+
+class UserActionAdmin(ExportActionModelAdmin):
+    #list_display = ['participant', 'participant__user__username', 'task', 'task__task_list__name']
+    list_display = ['__str__', 'user']
+    resource_class = UserActionResource
+
+
+admin.site.register(Balance, BalanceAdmin)
+admin.site.register(Message, MessageAdmin)
+admin.site.register(Result, ResultAdmin)
+admin.site.register(UserAction, UserActionAdmin)
+admin.site.register(QuestionnaireResponse, QuestionnaireResponseAdmin)
 admin.site.register(Profile)
 admin.site.register(Portfolio)
-admin.site.register(Balance)
-admin.site.register(Message)
-admin.site.register(UserAction)
 admin.site.register(Participant)
 admin.site.register(Condition)
 admin.site.register(DismissNotificationCount)
-admin.site.register(Result)
-admin.site.register(QuestionnaireResponse)
--- investment_bot/settings.py
+++ investment_bot/settings.py
@@ -79,6 +79,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'import_export',
     'mathfilters',
 
     'chatbot',
