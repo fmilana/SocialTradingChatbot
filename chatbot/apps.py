90f652f0a847af1c2dcdb3272e614dcadceeaeb8
Enrico Costanza <ec@ecs.soton.ac.uk>
2023-06-25 21:02:02 +0100

    updated code to run on django 4 tested only minimally

--- chatbot/apps.py
+++ chatbot/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ImagetaggingConfig(AppConfig):
+class ChatbotConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'imagetagging'
+    name = 'chatbot'
